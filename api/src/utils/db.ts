d3f2b48db82f9dc7c250c207a5c705978ae98cbe
= <=>
2024-01-27 13:42:12 +0530

    Update database configuration for docker runtime

--- api/src/utils/db.ts
+++ api/src/utils/db.ts
@@ -4,11 +4,11 @@ import mysql from "mysql";
 import { drizzle } from "drizzle-orm/mysql2";
 
 export const db_pool = mysql.createPool({
-  host: 'mysql',
-  database: process.env.DB || 'todo_schema',
-  port: parseInt(process.env.DB_PORT || '3306'),
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || 'password',
+  host: process.env.DB_HOST ,
+  database: process.env.DB ,
+  port: process.env.DB_PORT as number | undefined,
+  user: process.env.DB_USER ,
+  password: process.env.DB_PASSWORD ,
 });
 
 export const db_drizzel_pool = drizzle(db_pool);
